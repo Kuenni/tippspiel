7da9606cbc287211c2d5ff05065fef8f1d5281f8
Andreas Kuensken <andreas@kuensken-online.de>
2015-08-12 13:49:59 +0200

    Add files for main view and main controller

---
+++ api/controllers/MainController.js
@@ -0,0 +1,218 @@
+/**
+ * MainController
+ * 
+ * @description :: Server-side logic for managing mains
+ * @help :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index : function(req, res) {
+		res.view();
+	},
+	signup : function(req, res) {
+		var username = req.param("username");
+		var password = req.param("password");
+		Users.findByUsername(username).exec(function(err, usr) {
+			if (0)
+				console.log("if 0 ");
+			if (1)
+				console.log("if 1 ");
+			if (err) {
+				console.log("DB Error on signup");
+				res.send(500, {
+					error : "DB Error"
+				});
+			} else if (usr.length) {
+				console.log("Username taken");
+				res.send(400, {
+					error : "Username already Taken"
+				});
+			} else {
+				console.log("user: " + username);
+				var hasher = require("password-hash");
+				password = hasher.generate(password);
+				console.log("user: " + username);
+				console.log("pw: " + password);
+				Users.create({
+					username : username,
+					password : password
+				}).exec(function(error, user) {
+					if (error) {
+						res.send(500, {
+							error : "DB Error"
+						});
+					} else {
+						req.session.user = user;
+						res.send(user);
+					}
+				});
+			}
+		});
+	},
+	login : function(req, res) {
+		var username = req.param("username");
+		var password = req.param("password");
+
+		Users.findByUsername(username).exec(function(err, usr) {
+			if (err) {
+				console.log("DB Error");
+				res.send(500, {
+					error : "DB Error"
+				});
+			} else {
+				if (usr.length) {
+					user = usr[0]
+					var hasher = require("password-hash");
+					if (hasher.verify(password, user.password)) {
+						req.session.user = user;
+						res.send(user);
+					} else {
+						console.log("wrong pw");
+						res.statusCode = 400;
+						res.json({
+							error : "Wrong Password"
+						});
+					}
+				} else {
+					console.log("user unknown");
+					res.send(404, {
+						error : "User not Found"
+					});
+				}
+			}
+		});
+	},
+	results : function(req, res) {
+		var matchday = req.param("matchday");
+		if(typeof matchday == "undefined"){
+			matchday = 1
+		}
+		Results.findByMatchday(matchday).exec(
+				function(err, results) {
+					if (err) {
+						console.log("DB Error");
+						res.send(500, {
+							error : "DB Error"
+						});
+						return;
+					}
+					if (results.length == 0) {
+						res.send(500, {
+							error : "DB Empty"
+						});
+						return;
+					}
+					res.view({matches : results})
+				});
+
+	},
+	listMatchday : function(req, res) {
+		Results.findByMatchday(req.param("matchday")).exec(
+				function(err, results) {
+					if (err) {
+						console.log("DB Error");
+						res.send(500, {
+							error : "DB Error"
+						});
+					}
+					if (results.length == 0) {
+						res.send(500, {
+							error : "DB Empty"
+						});
+					}
+					res.send(200, {matches : results});
+				});
+	},
+	updateResults: function(req,res){
+		var matches = req.param("matches");
+		console.log(matches);
+		matches.forEach(function(match){
+			Results.update({id:match.id},
+					{goalshome:match.goalshome,goalsguest:match.goalsguest}).exec(function(err,updated){
+						if(err){
+							console.log('Error on update');
+							console.log(err);
+						} else{
+							console.log('Updated');
+						}
+					});
+		});
+		res.send(200);
+	},
+	addToDatabase : function(req, res) {
+		console.log("add to database");
+		if (req.method === 'GET')
+			return res.json({
+				'status' : 'GET not allowed'
+			});
+		// Call to /upload via GET is error
+		var file = req.file('jsonfile');
+
+		file.upload(function onUploadComplete(err, files) {
+			// Files will be uploaded to .tmp/uploads
+			if (err)
+				return res.serverError(err);
+			// IF ERROR Return and send 500 error with error
+
+			console.log(files[0]);
+			var uploadedFile = files[0].fd;
+			console.log(uploadedFile);
+			// try {
+			// var data = JSON.parse(files[0].fd);
+			// if came to here, then valid
+			// console.log(data);
+			// } catch(e) {
+			// failed to parse
+			// console.log("parse error")
+			// }
+			var matchesFile = require(files[0].fd);
+//			for (var i = 0; i < matchesFile.matchdays.length; i++) {
+			matchesFile.matchdays.forEach(function(match,index,array){
+//				match = matchesFile.matchdays[i];
+				console.log(match.matchday);
+				Results.find({matchday:match.matchday}).exec(
+						function(err, results) {
+							console.log(results);
+							if (err) {
+								console.log("DB Error in addMatchData");
+							}
+							if (results.length == 0) {
+								console.log("No data found for matchday");
+								 Results.create({
+								 matchday: match.matchday,
+								 teamhome: match.teamhome,
+								 teamguest: match.teamguest,
+								 goalshome: match.goalshome,
+								 goalsguest:
+								 match.goalsguest}).exec(console.log);
+							}
+							var foundMatch = false;
+							results.forEach(function(result,index2,array2){
+						//	for (var j = 0; j < results.length; j++) {
+						//		result = results[j];
+						//		console.log(result);
+								if (result.teamhome == match.teamhome) {
+									foundMatch = true;
+								}
+							});
+							//Das geht nicht. Durch async ist dieser Teil dran, bevor das Array
+							//durchlaufen wird
+							if (!foundMatch) {
+								console.log("TEst");
+//								Results.create({
+//									matchday : match.matchday,
+//									teamhome : match.teamhome,
+//									teamguest : match.teamguest,
+//									goalshome : match.goalshome,
+//									goalsguest : match.goalsguest
+//								}).exec(console.log);
+							}
+						});
+			});
+			res.json({
+				status : 200,
+				file : files
+			});
+		});
+	}
+};

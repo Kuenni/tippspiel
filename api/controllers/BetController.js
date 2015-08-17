325cc816e40bba96390339cc2b144de1986c67fa
Andreas Kuensken <andreas@kuensken-online.de>
2015-08-17 19:41:06 +0200

    Extend actions for listing and adding bets. Set default season to 2015 / 2016

--- api/controllers/BetController.js
+++ api/controllers/BetController.js
@@ -5,44 +5,85 @@
  * @help :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var SEASON = "2015/2016";
+
 module.exports = {
 
 		listBets : function(req, res) {
 			var matchday = req.param("matchday");
 			if(typeof matchday == "undefined"){
-				matchday = 1
+				matchday = 1;
 			}
-			Bets.findByMatchday(matchday).exec(
-					function(err, results) {
+			Bets.findByMatchday(matchday).exec(	function(err, bets) {
+				if (err) {
+					console.log("Bets DB Error");
+					res.send(500, {
+						error : "Bets DB Error"
+					});
+					return;
+				}
+				if(bets.length == 0){
+					Results.findByMatchday(matchday).exec(function(err, results) {
 						if (err) {
-							console.log("DB Error");
+							console.log("Results DB Error");
 							res.send(500, {
-								error : "DB Error"
+								error : "Results DB Error"
 							});
+							return;
 						}
-						var matchesLeft = results.length;
-						if(matchesLeft == 0){
-							res.send(200, {bets : results});
+						if (results.length == 0) {
+							res.send(500, {
+								error : "DB Empty"
+							});
+							return;
 						}
+						var resultsLeft = results.length;
 						results.forEach(function(match){
-							console.log('test')
-							delete match.id;
-							matchesLeft -= 1;
-							if(matchesLeft == 0){
-								res.send(200, {bets : results});
-							}
+							var newBet = match;
+							delete newBet.id;
+							match.goalshome = -1;
+							match.goalsguest = -1;
+							match.user = req.session.user.username;
+							match.season = SEASON;
+							Bets.create(match).exec(function(err,newItem){
+								if(err){
+									console.log("Error on creating new Bet")
+								}
+								resultsLeft -= 1;
+								console.log('created new bet');
+								console.log(newItem);
+								if(resultsLeft == 0){
+									Bets.findByMatchday(matchday).exec(function(err,results){
+										if (err) {
+											console.log("Results DB Error");
+											res.send(500, {
+												error : "Results DB Error"
+											});
+											return;
+										}else {
+											res.send(200, {bets : results});
+											return;
+										}
+									});
+								}
+							});
 						});
 					});
-		},
-		bets : function(req,res){
-			res.view();
+				}
+				else{
+					res.send(200, {bets : bets});
+				}
+			});
 		},
 		updateBets: function(req,res){
 			var matches = req.param("matches");
 			var matchesLeft = matches.length;
 			matches.forEach(function(match){
 				Bets.update({id:match.id},
-						{goalshome:match.goalshome,goalsguest:match.goalsguest}).exec(function(err,updated){
+						{	goalshome:match.goalshome,
+							goalsguest:match.goalsguest,
+							user:req.session.user.username,
+							season:SEASON }).exec(function(err,updated){
 							if(err){
 								console.log('Error on update');
 								console.log(err);
@@ -60,5 +101,8 @@ module.exports = {
 							}
 						});
 			});
+		},
+		bets : function(req,res){
+			res.view();
 		}
 };

0597f8090c63d8e6d093aaa13e6d8c5d9aead68f
Andreas Kuensken <andreas@kuensken-online.de>
2015-08-17 19:36:17 +0200

    Indentation

--- config/routes.js
+++ config/routes.js
@@ -3,40 +3,48 @@
 module.exports.routes = {
 
 
-	'/' : {
-		controller : 'main',
-		action : 'index'
-	},
-	'/results' : {
-		controller : 'main',
-		action : 'results'
-	},
-	'/loadData':{
-		view: 'main/loadData'
-	},
-	'/addToDatabase':{
-		controller: 'main',
-		action : 'addToDatabase'
-	},
-	'/listMatchday':{
-		controller: 'main',
-		action : 'listMatchday'
-	},
-	'/results/update':{
-		controller: 'main',
-		action: 'updateResults'
-	},
-	'/listBets':{
-		controller : 'bet',
-		action: 'listBets'
-	},
-	'/bets' : {
-		controller : 'bet',
-		action : 'bets'
-	},
-	'/bets/update' : {
-		controller : 'bet',
-		action : 'updateBets'
-	}
+		'/' : {
+			controller : 'main',
+			action : 'index'
+		},
+		'/signup' : {
+			controller: 'main',
+			action: 'signup'
+		},
+		'/login' : {
+			controller: 'main',
+			action: 'login'
+		},
+		'/results' : {
+			controller : 'main',
+			action : 'results'
+		},
+		'/loadData':{
+			view: 'main/loadData'
+		},
+		'/addToDatabase':{
+			controller: 'main',
+			action : 'addToDatabase'
+		},
+		'/listMatchday':{
+			controller: 'main',
+			action : 'listMatchday'
+		},
+		'/results/update':{
+			controller: 'main',
+			action: 'updateResults'
+		},
+		'/listBets':{
+			controller : 'bet',
+			action: 'listBets'
+		},
+		'/bets' : {
+			controller : 'bet',
+			action : 'bets'
+		},
+		'/bets/update' : {
+			controller : 'bet',
+			action : 'updateBets'
+		}
 
 };

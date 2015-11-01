276b8653cd9a391b3819b6c4bcede333be462632
Andreas Kuensken <andreas@kuensken-online.de>
2015-11-01 14:15:31 +0100

    Implement functions for number of difference and tendency bets

--- api/models/Users.js
+++ api/models/Users.js
@@ -10,30 +10,58 @@ module.exports = {
 		attributes: {
 			username: 'STRING',
 			password: 'STRING',
+			nCorrect: 'INT',
+			nDiff: 'INT',
+			nTrend: 'INT',
 			bets: {
 				collection: 'bets',
 				via : 'userMod'
 			},
 			getNumberCorrectBets : function(callback){
 				var correctCounter = 0;
-				Bets.find({user:this.user.username}).populateAll().exec(function(err,bets){
+				Bets.find({user:this.username}).populateAll().exec(function(err,bets){
 					if(err){
 						console.log("Get correct bets error");
 						console.log(err);
 					}
-					console.log("Counting correct Bets");
 					bets.forEach(function(bet){
 						if(bet.getBetResultCode() == 3){
 							correctCounter += 1;
 						}
-						console.log("count" + correctCounter);
 					});
-					console.log("Done");
 					callback(null,correctCounter);
 				});
 			},
-			getNumberDifferenceBets : function(){},
-			getNumberTendencyBets : function(){}
+			getNumberDifferenceBets : function(callback){
+				var differenceCounter = 0;
+				Bets.find({user:this.username}).populateAll().exec(function(err,bets){
+					if(err){
+						console.log("Get difference bets error");
+						console.log(err);
+					}
+					bets.forEach(function(bet){
+						if(bet.getBetResultCode() == 2){
+							differenceCounter += 1;
+						}
+					});
+					callback(null,differenceCounter);
+				});
+			},
+			getNumberTendencyBets : function(callback){
+				var tendencyCounter = 0;
+				Bets.find({user:this.username}).populateAll().exec(function(err,bets){
+					if(err){
+						console.log("Get difference bets error");
+						console.log(err);
+					}
+					bets.forEach(function(bet){
+						if(bet.getBetResultCode() == 1){
+							tendencyCounter += 1;
+						}
+					});
+					callback(null,tendencyCounter);
+				});
+			}
 		}
 };
 

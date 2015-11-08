19f2bcbfda2d10457ab44f5c34c790aa6201fde7
Andreas Kuensken <andreas@kuensken-online.de>
2015-11-08 20:01:53 +0100

    Call the remaining user ranking functions

--- api/services/Ranking.js
+++ api/services/Ranking.js
@@ -3,6 +3,8 @@ module.exports = {
 		User.find().populateAll().exec(function(err, users) {
 			users.forEach(function(user) {
 				user.updateNumberCorrectBets();
+				user.updateNumberDifferenceBets();
+				user.updateNumberTendencyBets();
 			});
 		});
 	}

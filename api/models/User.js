765888cf39c2ed9c7b6b37853c0b07ded77fb091
Andreas Kuensken <andreas@kuensken-online.de>
2015-11-10 17:17:48 +0100

    Difference and Correct counters seem to be working now

--- api/models/User.js
+++ api/models/User.js
@@ -18,63 +18,36 @@ module.exports = {
 				via : 'userMod'
 			},
 			updateNumberCorrectBets : function(){
-				console.log('Username in update ' + this.username);
 				var requestedUser = this;
 				var requestUsername = this.username;
 				Bet.find({user:this.username}).populateAll().exec(function(err,bets){
 					var correctCounter = 0;
-					console.log(requestedUser);
 					bets.forEach(function(bet){
-		//				console.log(bet);
-		//				console.log('###########')
 						var resultCode = bet.getBetResultCode();
-						console.log(resultCode);
-		//				console.log('###########')
 						if(resultCode == 3){
 							correctCounter += 1;
 						}
 						
 					});
 					requestedUser.nCorrect = correctCounter;
-					console.log('###########');
-					console.log(correctCounter);
-					console.log(requestedUser.nCorrect);
-					console.log(requestedUser.username);
-					console.log('###########');
-//					User.update({username : requestUsername},
-//							{nCorrect : correctCounter}).exec(function(err,updated){
-//								console.log(err);
-//								console.log(updated);
-//								console.log(requestUsername);
-//						//		updated[0].save();
-//							});
 					requestedUser.save();
 				});
-//				
-//				var correctCounter = 0;
-//				this.bets.forEach(function(bet){
-//					console.log(bet);
-//					if(bet.getBetResultCode() == 3){
-//						correctCounter += 1;
-//					}
-//				});
-//				this.nCorrect = correctCounter;
-//				this.save();
 			},
 			updateNumberDifferenceBets : function(){
+				var requestedUser = this;
 				var requestUsername = this.username;
 				Bet.find({user:this.username}).populateAll().exec(function(err,bets){
-
-				var differenceCounter = 0;
-				bets.forEach(function(bet){
-					if(bet.getBetResultCode() == 2){
-						differenceCounter += 1;
-					}
+					var differenceCounter = 0;
+					bets.forEach(function(bet){
+						var resultCode = bet.getBetResultCode();
+						console.log(resultCode);
+						if(resultCode == 2){
+							differenceCounter += 1;
+						}
+					});
+					requestedUser.nDiff = differenceCounter;
+					requestedUser.save();
 				});
-	//			this.user.nDiff = differenceCounter;
-	//			this.user.save();
-				});
-
 			},
 			updateNumberTendencyBets : function(callback){
 				Bet.find({user:this.username}).populateAll().exec(function(err,bets){

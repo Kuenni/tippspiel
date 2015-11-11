ca74764bffe130f8df929a0a63a491bf1c42f99a
Andreas Kuensken <andreas@kuensken-online.de>
2015-11-11 23:52:27 +0100

    Hide the login part on the main page if the user is already logged in

--- assets/js/mainPageApp.js
+++ assets/js/mainPageApp.js
@@ -5,6 +5,14 @@ mainPageApp.controller('MainPageController', function($scope, $http, $log) {
 	// $log is used for console log
 	// $http is used to communicate with the server
 	// $scope defines the scope of controller
+	var isUserLoggedIn = function(){
+		//Get the username for the logged-in user
+		$http.get('/username').success(function(data){
+			if(data.username){
+				$scope.$parent.loggedIn = true;
+			}
+		});
+	}
 	$scope.getRanking = function() {
 		$http({
 			method : "GET",
@@ -16,4 +24,5 @@ mainPageApp.controller('MainPageController', function($scope, $http, $log) {
 			alert("Fails");
 		});
 	}
+	isUserLoggedIn();
 });

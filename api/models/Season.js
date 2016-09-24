c17da88267986bd39dddbe3a076b2c9421805deb
Andreas Kuensken <andreas@kuensken-online.de>
2016-09-24 09:20:40 +0200

    Add Season model

---
+++ api/models/Season.js
@@ -0,0 +1,31 @@
+/**
+ * Season.js
+ *
+ * @description :: Model representation of openligadb data
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+		attributes: {
+			seasonName: {
+				type: 'STRING',
+				unique: true,
+				required: true,
+				minLength: 1
+			},
+			leagueShortcut:{
+				type: 'STRING',
+				unique: true,
+				required: true,
+				minLength: 1
+			},
+			leagueSeason: {
+				type: 'STRING',
+				unique: true,
+				required: true,
+				minLength: 1
+			}
+		}
+};
+

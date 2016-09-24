73e0d3613b643894f1071df8fc9dc496924fb2fb
Andreas Kuensken <andreas@kuensken-online.de>
2016-09-24 09:20:53 +0200

    Add Controller for Season

---
+++ api/controllers/SeasonController.js
@@ -0,0 +1,26 @@
+/**
+ * SeasonController
+ *
+ * @description :: Server-side logic for managing Seasons
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	'new': function(req,res) {
+			res.view();
+	},
+	create : function(req,res){
+		var params = _.extend(req.query || {}, req.params || {}, req.body || {});
+	  	Season.create(params, function seasonCreated (err, createdSeason) {
+	  		if (err){
+	  			console.log(err['reason']);
+	  			console.log();
+	  			console.log(err['details'])
+	  			req.session.messages['error'].push(err['details']);
+	  			return res.redirect('season/new');
+	  		}
+	  		return res.redirect('/season');
+	  	});
+	}
+};
+

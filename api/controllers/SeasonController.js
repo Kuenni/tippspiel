b7d3fdec768c9fe44ee6a311d32dcfb9d891b2e7
Andreas Kuensken <andreas@kuensken-online.de>
2016-09-27 20:55:55 +0200

    Code cleanup. Remove lots of unnecessary stuff

--- api/controllers/SeasonController.js
+++ api/controllers/SeasonController.js
@@ -13,9 +13,6 @@ module.exports = {
 		var params = _.extend(req.query || {}, req.params || {}, req.body || {});
 	  	Season.create(params, function seasonCreated (err, createdSeason) {
 	  		if (err){
-	  			console.log(err['reason']);
-	  			console.log();
-	  			console.log(err['details'])
 	  			req.session.messages['error'].push(err['details']);
 	  			return res.redirect('season/new');
 	  		}

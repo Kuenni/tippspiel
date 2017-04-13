6aa6f03133d691e203eed6a71941cb8ef0cba31d
Andreas Kuensken <andreas@kuensken-online.de>
2017-04-13 09:14:41 +0200

    Add function for retrieving matches of a given matchday and season

--- api/services/LigaDbCaller.js
+++ api/services/LigaDbCaller.js
@@ -69,5 +69,68 @@ module.exports = {
 						callback(0,JSON.parse(body).GroupOrderID);
 					}
 				});
+		},
+		
+		getMatchesOfTheDay : function getMatchesOfDay(matchday, season,callback){
+			var matchday = matchday;
+			var sesason = season;
+			sails.log.info("Getting matches of the day");
+			Match.find({matchday:matchday,season:season.id}).populateAll().exec(
+					function(err,matches){
+						if(err){
+							sails.log.error("Error finding matches for the match day");
+							sails.log.error(err);
+							return null;
+						}
+						/*
+						 * No Matches found in match database
+						 */
+						if(matches.length == 0){
+							sails.log.debug("No matches in database yet");
+								// Get Match data from online DB
+								LigaDbCaller.getMatches(season, matchday, function(err, dbMatches){
+									if(err){
+										sails.log.error("Error getting matches from the Liga DB");
+										sails.log.error(err);
+										return null;
+									}
+									var matchesToProcess = dbMatches.length;
+									var matchReturnList = [];
+									// Create a new Match entry for each match from online DB
+									dbMatches.forEach(function(match){
+										Match.create({
+											matchday		: matchday,
+											openDbMatchId	: match.MatchID,
+											season			: season,
+											teamhome		: match.Team1.TeamName,
+											teamguest		: match.Team2.TeamName,
+											logoTeam1		: match.Team1.TeamIconUrl,
+											logoTeam2		: match.Team2.TeamIconUrl,
+											matchDateTime	: match.MatchDateTime,
+											isFinished		: match.MatchIsFinished
+										}).exec(function(err,createdMatch){
+											if(err){
+												sails.log.error("Error on creating new match");
+												sails.log.error(err);
+												return null;
+											}
+											matchesToProcess -= 1;
+											matchReturnList.push(createdMatch);
+											if(matchesToProcess == 0){
+												return matchReturnList;
+											}
+										});
+									});
+								});
+						}//if matches length == 0
+						/*
+						 * Matches already available in DB
+						 */
+						else{
+							// If there are already matches available, just send them
+							return matches;
+						}
+					}
+			);
 		}
 }

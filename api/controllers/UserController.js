316f5c2f95ad61d0d8b8566e22c65d9a670348dd
Andreas Kuensken <andreas@kuensken-online.de>
2017-08-25 22:03:11 +0200

    Move the point calculation to a dedicated action to free the index action of the User Controller.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,41 +5,64 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-		index : function(req,res){
-			var userPoints = []
-			User.find().populate("bets").exec(function(err,userList){
-				var usersLeft = userList.length;
-				userList.forEach(function(user){
-					var localObject = {username:user.username,points:0,nCorrect : 0, nDiff : 0, nTrend: 0};
-					req.query.user = user.id;
-					Bet.find(req.query).populate("season").exec(function(err,betList){
-						usersLeft -= 1;
-						betList.forEach(function(bet){
-							localObject.points += +bet._points();
-							switch (+bet._points()) {
-							case 5:
-								localObject.nCorrect += 1;
-								break;
-							case 3:
-								localObject.nDiff += 1;
-								break;
-							case 1:
-								localObject.nTrend += 1;
-								break;
-							default:
-								break;
-							}
-						});
-						userPoints.push(localObject);
-						if(usersLeft == 0){
-							return res.json(userPoints);
-							console.log(userPoints);
-						}
-					});
-				});
+var addPointsForUser = function(season){
+	return function(user,callback){
+		var pointCounterObject 
+		= {username:user.username,points:0,nCorrect : 0, nDiff : 0, nTrend: 0};
+		Bet.find({user:user.id,season:season})
+		.then(function(bets){
+			bets.forEach(function(bet){			// For all bets for a user
+				var betPoints = +bet._points();	// sum up the points
+				if( isNaN(betPoints) ){			// and skip NaNs
+					return;
+				}
+				pointCounterObject.points += betPoints;
+				switch (+bet._points()) { //unary + means cast to integer
+				case 5:
+					pointCounterObject.nCorrect += 1;
+					break;
+				case 3:
+					pointCounterObject.nDiff += 1;
+					break;
+				case 1:
+					pointCounterObject.nTrend += 1;
+					break;
+				default:
+					break;
+				}
 			});
+			return callback(0,pointCounterObject);
+		})
+		.catch(function(err){
+			return callback(err);
+		});
+	}
+}
+
+module.exports = {
+		ranking : function(req,res){
+			if(req.query.season){
+				var userPoints = [];
+				User.find().populate('bets')
+				.then(function(users){
+					async.map(users,addPointsForUser(req.query.season),function(err,pointList){
+						if(err){
+							sails.log.error("UserController - ranking: Error in mapping users and points");
+							sails.log.error(err);
+							return res.send(500,err);
+						}
+						return res.json(pointList);
+					});
+				})
+				.catch(function(err){
+					sails.log.error("UserController - ranking: Error in finding users");
+					sails.log.error(err);
+					return res.send(500,err);
+				});
+
+			} else{
+				return res.send(400,{message:"Need a season to create the ranking for!"});
+			}
 		}
-		
 };
 
